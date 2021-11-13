d49dc9d7648acbb25e77f881e331a36aa2883dac
Maroš Hajník <hajnik.work@gmail.com>
2021-11-13 12:18:42 +0100

    Created assets folder with global styles and variables. Installed node-sass.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Global styles
+import './assets/styles/main.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
